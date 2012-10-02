1b735cd3fc3b3dd1b953e911073118276bc281d4
John Barnette <john@jbarnette.com>
2012-10-02 16:24:46 -0700

    Boxen.run -> Boxen::CLI.run

--- lib/boxen/cli.rb
+++ lib/boxen/cli.rb
@@ -1,4 +1,8 @@
-require "boxen"
+require "boxen/config"
+require "boxen/flags"
+require "boxen/postflight"
+require "boxen/preflight"
+require "boxen/puppeteer"
 
 module Boxen
   class CLI
@@ -10,6 +14,43 @@ module Boxen
       @config = config
       @flags  = flags
       @puppet = Boxen::Puppeteer.new @config
+    end
+
+    def run
+      warn "Finna run."
+      0 # FIX: puppet exit code
+    end
+
+    # Run Boxen by wiring together the command-line flags, config,
+    # preflights, Puppet execution, and postflights. Returns Puppet's
+    # exit code.
+
+    def self.run(*args)
+      config = Boxen::Config.load
+      flags  = Boxen::Flags.new args
+
+      # Apply command-line flags to the config in case we're changing or
+      # overriding anything.
+
+      flags.apply config
+
+      # Run the preflight checks.
+
+      Boxen::Preflight.run config
+
+      # Save the config for Puppet (and next time).
+
+      Boxen::Config.save config
+
+      # Make the magic happen.
+
+      code = Boxen::CLI.new(config, flags).run
+
+      # Run the postflight checks.
+
+      Boxen::Postflight.run config if code.zero?
+
+      return code
     end
   end
 end
--- test/boxen_test.rb
+++
@@ -1,8 +0,0 @@
-require "boxen/test"
-require "boxen"
-
-class BoxenTest < Boxen::Test
-  def test_self_run
-    # FIX
-  end
-end
