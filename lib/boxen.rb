487545cf3af3c51a1f67bb3e2bc086a48b337e60
John Barnette <jbarnette@github.com>
2012-10-02 09:57:41 -0700

    Add helpers to check active state and run sudo

--- lib/boxen.rb
+++ lib/boxen.rb
@@ -1,2 +1,14 @@
 module Boxen
+
+  # Is Boxen active?
+
+  def self.active?
+    ENV.include? "BOXEN_HOME"
+  end
+
+  # Run `args` as a system command with sudo if necessary.
+
+  def self.sudo *args
+    system "sudo", "-p", "Password for sudo: ", *args
+  end
 end
--- test/boxen_test.rb
+++ test/boxen_test.rb
@@ -2,7 +2,18 @@ require "boxen/test"
 require "boxen"
 
 class BoxenTest < Boxen::Test
-  def test_sanity
-    assert Boxen
+  def test_self_active?
+    ENV.expects(:include?).with("BOXEN_HOME").returns true
+    assert Boxen.active?
+  end
+
+  def test_self_active_disabled
+    ENV.expects(:include?).with("BOXEN_HOME").returns false
+    refute Boxen.active?
+  end
+
+  def test_self_sudo
+    Boxen.expects(:system).with "sudo", "-p", "Password for sudo: ", "echo", "foo"
+    Boxen.sudo "echo", "foo"
   end
 end
