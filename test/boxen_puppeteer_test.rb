1002900cbdf3ca36cdb75acfd2e39c7d75cbfe52
Will Farrington <wfarr@github.com>
2012-10-03 16:49:38 -0700

    add assertion for group flag in puppet run

--- test/boxen_puppeteer_test.rb
+++ test/boxen_puppeteer_test.rb
@@ -29,6 +29,7 @@ class BoxenPuppeteerTest < Boxen::Test
     assert_flag "--summarize", flags
 
     assert_flag_value "--confdir", :anything, flags
+    assert_flag_value "--group", "admin", flags
     assert_flag_value "--vardir", :anything, flags
     assert_flag_value "--libdir", :anything, flags
     assert_flag_value "--manifestdir", :anything, flags
