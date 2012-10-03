3b43cb0f8eae42b461b9f567d5aac042037a0354
Mike Skalnik <mike.skalnik@gmail.com>
2012-10-03 11:36:29 -0400

    Add RVM preflight check

---
+++ lib/boxen/preflight/rvm.rb
@@ -0,0 +1,12 @@
+require "boxen/preflight"
+
+class Boxen::Preflight::Rvm < Boxen::Preflight
+  def run
+    warn "You have an rvm installed in ~/.rvm.",
+      "The Setup uses rbenv to install ruby, so consider `rvm implode`ing"
+  end
+
+  def ok?
+    !File.exist? "#{ENV['HOME']}/.rvm"
+  end
+end
