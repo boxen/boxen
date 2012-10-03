ae4a3a31cf0c02456620ddf3fa8ed45f3d4535e5
Mike Skalnik <mike.skalnik@gmail.com>
2012-10-03 15:28:18 -0400

    Check Preflight::RVM#ok? return as well

--- test/boxen_preflight_rvm_test.rb
+++ test/boxen_preflight_rvm_test.rb
@@ -4,7 +4,7 @@ require 'boxen/preflight/rvm'
 class BoxenPreflightRVMTest < Boxen::Test
   def test_directory_check
     preflight = Boxen::Preflight::RVM.new(mock('config'))
-    File.expects(:exist?).with("#{ENV['HOME']}/.rvm")
-    preflight.ok?
+    File.expects(:exist?).with("#{ENV['HOME']}/.rvm").returns(false)
+    assert preflight.ok?
   end
 end
