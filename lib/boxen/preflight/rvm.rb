6ac25b80e4158909ede259d7dbbfcfee292a51a1
Mike Skalnik <mike.skalnik@gmail.com>
2012-10-03 13:42:53 -0400

    Abort if RVM is found

--- lib/boxen/preflight/rvm.rb
+++ lib/boxen/preflight/rvm.rb
@@ -2,8 +2,8 @@ require "boxen/preflight"
 
 class Boxen::Preflight::RVM < Boxen::Preflight
   def run
-    warn "You have an rvm installed in ~/.rvm.",
-      "The Setup uses rbenv to install ruby, so consider `rvm implode`ing"
+    abort "You have an rvm installed in ~/.rvm.",
+      "The Setup uses rbenv to install ruby, so please `rvm implode`"
   end
 
   def ok?
