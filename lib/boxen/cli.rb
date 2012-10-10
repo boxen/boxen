26e28997281cc0ee354304a5e646c996ec529f85
Yossef Mendelssohn <ymendel@pobox.com>
2012-10-10 12:29:51 -0400

    Extract CLI#run to a Runner class

--- lib/boxen/cli.rb
+++ lib/boxen/cli.rb
@@ -1,103 +1,24 @@
-require "boxen/checkout"
 require "boxen/config"
 require "boxen/flags"
 require "boxen/postflight"
 require "boxen/preflight"
-require "boxen/puppeteer"
-require "boxen/reporter"
+require "boxen/runner"
 require "boxen/util"
 
 module Boxen
   class CLI
     attr_reader :config
     attr_reader :flags
-    attr_reader :puppet
-    attr_reader :checkout
-    attr_reader :reporter
+    attr_reader :runner
 
     def initialize(config, flags)
       @config = config
       @flags  = flags
-      @puppet = Boxen::Puppeteer.new @config
-      @checkout = Boxen::Checkout.new(@config)
-      @reporter = Boxen::Reporter.new(@config, @checkout, @puppet)
-    end
-
-    def process
-      # --env prints out the current BOXEN_ env vars.
-
-      exec "env | grep ^BOXEN_ | sort" if flags.env?
-
-      # --help prints some CLI help and exits.
-
-      abort "#{flags}\n" if flags.help?
-
-      # --projects prints a list of available projects and exits.
-
-      if flags.projects?
-        config.projects.each do |project|
-          prefix = project.installed? ? "*" : " "
-          puts "#{prefix} #{project.name}"
-        end
-
-        exit
-      end
-
-      # --disable-services stops all services
-
-      if flags.disable_services?
-        Dir["/Library/LaunchDaemons/com.boxen.*.plist"]. each do |service|
-          service_human_name = service.match(/com\.boxen\.(.+)\.plist$/)[1]
-          puts "Disabling #{service_human_name}..."
-          Boxen::Util.sudo("/bin/launchctl", "unload", "-w", service)
-        end
-
-        exit
-      end
-
-      # --enable-services starts all services
-
-      if flags.enable_services?
-        Dir["/Library/LaunchDaemons/com.boxen.*.plist"]. each do |service|
-          service_human_name = service.match(/com\.boxen\.(.+)\.plist$/)[1]
-          puts "Enabling #{service_human_name}..."
-          Boxen::Util.sudo("/bin/launchctl", "load", "-w", service)
-        end
-
-        exit
-      end
-
-      # --list-services lists all services
-
-      if flags.list_services?
-        Dir["/Library/LaunchDaemons/com.boxen.*.plist"]. each do |service|
-          service_human_name = service.match(/com\.boxen\.(.+)\.plist$/)[1]
-          puts service_human_name
-        end
-
-        exit
-      end
-
-      # Actually run Puppet and return its exit code.
-
-      puppet.run
+      @runner = Boxen::Runner.new(@config, @flags)
     end
 
     def run
-      report(process)
-    end
-
-    def report(result)
-      return result unless issues?
-
-      if self.class.successful_exit_code? result
-        reporter.close_failures
-      else
-        warn "Sorry! Creating an issue on #{config.reponame}."
-        reporter.record_failure
-      end
-
-      result
+      runner.run
     end
 
     # Run Boxen by wiring together the command-line flags, config,
@@ -143,12 +64,6 @@ module Boxen
 
     def self.successful_exit_code?(code)
       [0, 2].member? code
-    end
-
-    # Should the result of this run have any effect on GitHub issues?
-
-    def issues?
-      !config.stealth? && !config.pretend? && checkout.master?
     end
   end
 end
---
+++ test/boxen_runner_test.rb
@@ -0,0 +1,183 @@
+require "boxen/test"
+require "boxen/runner"
+
+class BoxenRunnerTest < Boxen::Test
+  def setup
+    @config = Boxen::Config.new
+    @flags  = Boxen::Flags.new
+    @runner = Boxen::Runner.new(@config, @flags)
+
+    $stdout.stubs(:puts).returns(true)
+    $stdout.stubs(:write).returns(true)
+  end
+
+  def test_initialize
+    config = Boxen::Config.new
+    flags  = Boxen::Flags.new
+
+    runner = Boxen::Runner.new(config, flags)
+
+    assert_equal config, runner.config
+    assert_equal flags,  runner.flags
+    assert_equal config, runner.puppet.config
+
+    assert_equal config,          runner.reporter.config
+    assert_equal config,          runner.reporter.checkout.config
+    assert_equal runner.checkout, runner.reporter.checkout
+    assert_equal runner.puppet,   runner.reporter.puppet
+  end
+
+  def test_issues?
+    @config.stealth = true
+    @config.pretend = true
+    @runner.checkout.stubs(:master?).returns(false)
+    assert !@runner.issues?
+
+    @config.stealth = false
+    @config.pretend = true
+    @runner.checkout.stubs(:master?).returns(false)
+    assert !@runner.issues?
+
+    @config.stealth = true
+    @config.pretend = false
+    @runner.checkout.stubs(:master?).returns(false)
+    assert !@runner.issues?
+
+    @config.stealth = true
+    @config.pretend = true
+    @runner.checkout.stubs(:master?).returns(true)
+    assert !@runner.issues?
+
+    @config.stealth = false
+    @config.pretend = true
+    @runner.checkout.stubs(:master?).returns(true)
+    assert !@runner.issues?
+
+    @config.stealth = true
+    @config.pretend = false
+    @runner.checkout.stubs(:master?).returns(true)
+    assert !@runner.issues?
+
+    @config.stealth = false
+    @config.pretend = false
+    @runner.checkout.stubs(:master?).returns(true)
+    assert @runner.issues?
+  end
+
+  def test_report_failure
+    @runner.stubs(:issues?).returns(true)
+    @runner.stubs(:process).returns(1)
+    @runner.stubs(:warn)
+
+    @runner.reporter.expects(:record_failure)
+    @runner.reporter.expects(:close_failures).never
+
+    @runner.run
+  end
+
+  def test_run_success
+    @runner.stubs(:issues?).returns(true)
+    @runner.stubs(:process).returns(0)
+
+    @runner.reporter.expects(:record_failure).never
+    @runner.reporter.expects(:close_failures)
+
+    @runner.run
+  end
+
+  def test_run_success_exit_code_2
+    @runner.stubs(:issues?).returns(true)
+    @runner.stubs(:process).returns(2)
+
+    @runner.reporter.expects(:record_failure).never
+    @runner.reporter.expects(:close_failures)
+
+    @runner.run
+  end
+
+  def test_run_failure_no_issues
+    @runner.stubs(:issues?).returns(false)
+    @runner.stubs(:process).returns(1)
+
+    @runner.reporter.expects(:record_failure).never
+    @runner.reporter.expects(:close_failures).never
+
+    @runner.run
+  end
+
+  def test_run_success_no_issues
+    @runner.stubs(:issues?).returns(false)
+    @runner.stubs(:process).returns(0)
+
+    @runner.reporter.expects(:record_failure).never
+    @runner.reporter.expects(:close_failures).never
+
+    @runner.run
+  end
+
+  def test_run_success_exit_code_2_no_issues
+    @runner.stubs(:issues?).returns(false)
+    @runner.stubs(:process).returns(2)
+
+    @runner.reporter.expects(:record_failure).never
+    @runner.reporter.expects(:close_failures).never
+
+    @runner.run
+  end
+
+  def test_disable_services
+    config = Boxen::Config.new
+    flags  = Boxen::Flags.new('--disable-services')
+    runner = Boxen::Runner.new config, flags
+
+    Dir.expects(:[]).with("/Library/LaunchDaemons/com.boxen.*.plist").returns([
+      "/Library/LaunchDaemons/com.boxen.test.plist"
+    ])
+
+    Boxen::Util.expects(:sudo).with(
+      "/bin/launchctl",
+      "unload",
+      "-w",
+      "/Library/LaunchDaemons/com.boxen.test.plist"
+    ).returns(true)
+
+    assert_raises(SystemExit) do
+      runner.process
+    end
+  end
+
+  def test_enable_services
+    config = Boxen::Config.new
+    flags  = Boxen::Flags.new('--enable-services')
+    runner = Boxen::Runner.new config, flags
+
+    Dir.expects(:[]).with("/Library/LaunchDaemons/com.boxen.*.plist").returns([
+      "/Library/LaunchDaemons/com.boxen.test.plist"
+    ])
+
+    Boxen::Util.expects(:sudo).with(
+      "/bin/launchctl",
+      "load",
+      "-w",
+      "/Library/LaunchDaemons/com.boxen.test.plist"
+    ).returns(true)
+
+    assert_raises(SystemExit) do
+      runner.process
+    end
+  end
+
+  def test_list_services
+    config = Boxen::Config.new
+    flags  = Boxen::Flags.new('--list-services')
+    runner = Boxen::Runner.new config, flags
+
+    Dir.expects(:[]).with("/Library/LaunchDaemons/com.boxen.*.plist").returns([
+      "/Library/LaunchDaemons/com.boxen.test.plist"
+    ])
+
+    assert_raises(SystemExit) do
+      runner.process
+    end
+  end
+end
