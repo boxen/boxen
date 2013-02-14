cee8c6e90404850a2834d6fa2ffc8d1907bd1e0a
Will Farrington <wfarr@github.com>
2013-02-13 16:35:24 -0800

    spike at adding write support to our keychain helper

--- src/keychain-helper.c
+++ src/keychain-helper.c
@@ -5,9 +5,10 @@
 int main(int argc, char **argv) {
   const char *service = argv[1];
   const char *login   = argv[2];
+  const void *password = argv[3];
 
-  if (!(argv[1] && argv[2])) {
-    printf("Usage: %s <service> <account>\n", argv[0]);
+  if (!(service && login) || !password) {
+    printf("Usage: %s <service> <account> [<password>]\n", argv[0]);
     exit(1);
   }
 
@@ -15,13 +16,25 @@ int main(int argc, char **argv) {
   UInt32 len;
   SecKeychainItemRef item;
 
-  int ret = SecKeychainFindGenericPassword(
-    NULL, strlen(service), service, strlen(login), login, &len, &buf, &item);
+  if (password) {
+    int ret = SecKeychainAddGenericPassword(
+      NULL, strlen(service), service, strlen(login), login, strlen(password),
+      password, &item
+    );
 
-  if (ret) {
-    exit(1);
+    if (ret) {
+      exit(1);
+    }
+  } else {
+    int ret = SecKeychainFindGenericPassword(
+      NULL, strlen(service), service, strlen(login), login, &len, &buf, &item);
+
+    if (ret) {
+      exit(1);
+    }
+
+    fwrite(buf, 1, len, stdout);
   }
 
-  fwrite(buf, 1, len, stdout);
   exit(0);
 }
