af20267ac6c4b9e5b99a95504df6eb7e1156278d
Ben Toews <mastahyeti@gmail.com>
2013-09-03 16:28:42 -0600

    try to get at the stderr

--- lib/boxen/keychain.rb
+++ lib/boxen/keychain.rb
@@ -44,8 +44,9 @@ module Boxen
     def set(service, token)
       cmd = shellescape(HELPER, service, login, token)
 
-      unless system *cmd
-        raise Boxen::Error, "Can't save #{service} in the keychain."
+      result = `cmd`
+      unless $?.success?
+        raise Boxen::Error, "Can't save #{service} in the keychain.\nError: #{result}"
       end
 
       token
