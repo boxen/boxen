957119345273b8927e7802a1d23276159b1bf1ba
Ben Toews <mastahyeti@gmail.com>
2013-09-03 17:58:37 -0600

    skip better

--- test/boxen_keychain_test.rb
+++ test/boxen_keychain_test.rb
@@ -1,20 +1,24 @@
 require "boxen/test"
 require "boxen/keychain"
 
-if RUBY_PLATFORM.downcase.include?("darwin")
-  class BoxenKeychainTest < Boxen::Test
-    def setup
-      @keychain = Boxen::Keychain.new('test')
-    end
+class BoxenKeychainTest < Boxen::Test
+  def setup
+    @keychain = Boxen::Keychain.new('test')
+  end
 
-    def test_get_token
-      @keychain.expects(:get).with('GitHub API Token').returns('foobar')
-      assert_equal 'foobar', @keychain.token
-    end
+  def osx?
+    RUBY_PLATFORM.downcase.include?("darwin")
+  end
 
-    def test_set_token
-      @keychain.expects(:set).with('GitHub API Token', 'foobar').returns('foobar')
-      assert_equal 'foobar', @keychain.token=('foobar')
-    end
+  def test_get_token
+    skip("Keychain helper is OSX only") unless osx?
+    @keychain.expects(:get).with('GitHub API Token').returns('foobar')
+    assert_equal 'foobar', @keychain.token
+  end
+
+  def test_set_token
+    skip("Keychain helper is OSX only") unless osx?
+    @keychain.expects(:set).with('GitHub API Token', 'foobar').returns('foobar')
+    assert_equal 'foobar', @keychain.token=('foobar')
   end
 end
