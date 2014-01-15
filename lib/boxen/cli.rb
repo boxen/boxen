57744b6eadf933b3be47c4b53b80fb011ddfe17b
David Goodlad <david@goodlad.net>
2014-01-15 15:06:30 +1100

    Fix cli on 1.8.7
    I can't wait to :burn: 1.8.7 but too many people are still on OSX 10.8

--- lib/boxen/cli.rb
+++ lib/boxen/cli.rb
@@ -4,7 +4,7 @@ require "boxen/config"
 module Boxen
   class CLI
     def self.run(*args)
-      cmd, cmd_args = args.flatten
+      cmd, *cmd_args = args.flatten
       config = Boxen::Config.load
       status = Boxen::Command.invoke cmd, config, *cmd_args
 
