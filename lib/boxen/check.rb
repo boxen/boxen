c8edf2d790bbbf1e5e85501ab1a3a6f130a0cc2d
Sean Knox <knoxville@gmail.com>
2015-03-20 23:20:12 -0700

    Refactor cli.rb into smaller methods, style fixes.

--- lib/boxen/check.rb
+++ lib/boxen/check.rb
@@ -1,29 +1,23 @@
-require "ansi"
+require 'ansi'
 
 module Boxen
-
   # The superclass for preflight and postflight sanity checks.
-
   class Check
-
     # A collection of preflight instances for `config`. An instance is
     # created for every constant under `self` that's also a
     # subclass of `self`.
-
     def self.checks(config)
-      constants.map { |n| const_get n }.
-        select { |c| c < self }.
-        map { |c| c.new config }
+      constants.map { |n| const_get n }
+        .select { |c| c < self }
+        .map { |c| c.new config }
     end
 
     # Search `dir` and load all Ruby files under it.
-
     def self.register(dir)
       Dir["#{dir}/*.rb"].sort.each { |f| load f }
     end
 
     # Check each instance against `config`.
-
     def self.run(config)
       checks(config).each { |check| check.run unless check.ok? }
     end
@@ -35,15 +29,14 @@ module Boxen
     end
 
     # Is everything good to go? Implemented by subclasses.
-
     def ok?
-      raise "Subclasses must implement this method."
+      fail 'Subclasses must implement this method.'
     end
 
     # Warn, fix, or abort. Implemented by subclasses.
 
     def run
-      raise "Subclasses must implement this method."
+      fail 'Subclasses must implement this method.'
     end
 
     # A fancier `abort` and `warn`. This will probably really annoy
@@ -51,7 +44,7 @@ module Boxen
     # but it's limited to checks.
 
     def abort(message, *extras)
-      extras << { :color => :red }
+      extras << { color: :red }
       warn message, *extras
       exit 1
     end
@@ -62,9 +55,7 @@ module Boxen
 
       $stderr.puts ANSI.send(color) { "--> #{message}" }
 
-      unless extras.empty?
-        extras.each { |line| $stderr.puts "    #{line}" }
-      end
+      extras.each { |line| $stderr.puts "    #{line}" } unless extras.empty?
 
       $stderr.puts
     end
