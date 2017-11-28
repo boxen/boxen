a7fa4a4c8f289d6b52d5096398634afbd94ad39f
n0ts <naoya.n@gmail.com>
2017-11-28 15:24:38 -0800

    Keychain is running only macOS

--- lib/boxen/util.rb
+++ lib/boxen/util.rb
@@ -2,16 +2,18 @@ module Boxen
   module Util
 
     # Is Boxen active?
-
     def self.active?
       ENV.include? "BOXEN_HOME"
     end
 
-
     # Run `args` as a system command with sudo if necessary.
-
     def self.sudo *args
       system "sudo", "-p", "Password for sudo: ", *args
+    end
+
+    # Run on macos?
+    def self.osx?
+      RUBY_PLATFORM.downcase.include?('darwin')
     end
   end
 end
